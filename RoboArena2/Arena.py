948f0c3edca984e969be35bd15dca9726e278dcc
roadsidepicnic1 <92788935+roadsidepicnic1@users.noreply.github.com>
2023-05-01 16:24:46 +0200

    Update Arena.py

--- RoboArena2/Arena.py
+++ RoboArena2/Arena.py
@@ -1,54 +1,49 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QColor
 
 
-class Arena:
+class Arena(QMainWindow):     #Erbt von QMainWindow class, allows to use methods like setWindowTitle directly...
     def __init__(self):
         super().__init__()
-        self.width = 1000      #When a new instance is initialized, width and height is automatically set to 1000
+        self.width = 1000
         self.height = 1000
-        self.tiles = [[0 for i in range(100)] for j in range(100)] # creates 2 dimensional Array
+        self.tiles = [[0 for i in range(100)] for j in range(100)]
+        self.title = "RoboArena"
+        self.top = 0
+        self.left = 0
 
-    def get_size(self):        #method to print actual size of the arena
-        print(f"The arena size is {self.width}x{self.height} pixels.")
+    def get_size(self):                     #method to print actual size of the arena
+        print(f"The arena size is {self.width}x{self.height} pixels and positioned top {self.top}, left {self.left}.")
 
-    def change_size(self, w, h): #method to change size of the arena
-        self.width = w
-        self.height = h
-
-    def set_tile(self,x,y, type): #changes the type of tile in the included coordinate
+    def set_tile(self,x,y, type):       #changes the type of tile in the included coordinate
         self.tiles[y][x] = type
 
-    def get_tile(self,x, y):    # return the type of the tile in the included coordinate
+    def get_tile(self,x, y):            # return the type of the tile in the included coordinate
         return self.tiles[y][x]
 
-    def draw(self):  # draw the arena, the code is the same like in the tutorial program
-        app = QApplication(sys.argv)
+    def InitWindow(self ):              #Displays the Arena
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
 
-        w = QWidget()
-        w.resize(self.width, self.height)
-        w.setWindowTitle('Arena')
-        w.show()
-        drawer = QPainter(w)
-
-        for y in range(len(self.tiles)):
-            for x in range(len(self.tiles[y])):
-                tile_type = self.tiles[y][x]
-                if tile_type == "wall":
-                    drawer.setBrush(QColor(45, 255, 255))
-                    drawer.drawRect(x * 10, y * 10, 10, 10)
-
-        sys.exit(app.exec_())
+    def paintEvent(self, event):        #Colors the tiles
+        painter = QPainter(self)
+        for y in range(0,100):          #Iterates through every possible tile
+            for x in range(0,100):
+                if self.tiles[y][x] == "Wall":      #Depending on the type of the tile, different drawings/colors
+                    painter.setPen(QPen(Qt.green, 8, Qt.DashLine))                 #are used
+                    painter.drawRect(x*10, y*10, 10, 10)
 
 
-test_arena = Arena()
-test_arena.set_tile(4,7, "wall")
-test_arena.set_tile(20,7, "wall")
-test_arena.set_tile(10,7, "wall")
-test_arena.set_tile(50,20, "wall")
-print(test_arena.get_tile(4,7))
-test_arena.draw()
 
-
+App = QApplication(sys.argv)
+testarena = Arena()
+testarena.set_tile(50,50, "Wall")
+testarena.set_tile(30,30, "Wall")
+testarena.set_tile(10,10, "Wall")
+testarena.InitWindow()
+sys.exit(App.exec())
 
