f58d751d666e9bc6deae77da835cc95bf04fab48
Marcel John <marcel.john.s@t-online.de>
2023-05-03 19:19:09 +0200

    added move function in BasicRobot

--- RoboArena2/BasicRobot.py
+++ RoboArena2/BasicRobot.py
@@ -10,3 +10,8 @@ class BasicRobot:
         self.y = yPos
         self.radius = rad
         self.alpha = -dir
+    
+    #cos(a)^2+sin(a)^2=1 that is why we use this for movement
+    def move(self):
+        self.x = self.x + math.cos(self.radius) ** 2
+        self.y = self.y + math.sin(self.radius) ** 2
