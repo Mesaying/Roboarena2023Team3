ed7e887f022ce96393ab50118423a524a02bda86
roadsidepicnic1 <92788935+roadsidepicnic1@users.noreply.github.com>
2023-05-08 09:57:37 +0200

    Fixed drawing arena
    leerzeilen und zeilenumbrüche werden nun ignoriert

--- RoboArena2/Arena.py
+++ RoboArena2/Arena.py
@@ -54,6 +54,7 @@ class Arena(
         list_with_tiles = []
         with open("testarena.txt", "r") as file:  # Opens the textfile
             content = file.read()
+            content = content.replace(" ", "").replace("\n", "")  #ignoriert zeilenumbrüche und leerzeichen
         for letter in content:  # saves every letter in a list
             list_with_tiles.append(letter)
 
