8f4e8532e35562c3ad32c09a572d416daeba4c49
roadsidepicnic1 <roadsidepicnic1@users.noreply.github.com>
2023-05-22 12:24:02 +0000

    Format code with black

--- RoboArena2/MapEditor.py
+++ RoboArena2/MapEditor.py
@@ -1,8 +1,8 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt, QRect
 import sys
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QPainter, QPen
+from PyQt5.QtWidgets import *
 
 
 class MapEditor(QWidget):
@@ -10,7 +10,7 @@ class MapEditor(QWidget):
         super().__init__()
 
         # title and size of window
-        self.setWindowTitle('Map Editor')
+        self.setWindowTitle("Map Editor")
         self.setGeometry(100, 100, 1000, 1000)
 
         # create the main layout
@@ -19,46 +19,48 @@ class MapEditor(QWidget):
 
         # create buttons
         self.draw_mode = None
-        water_button = QPushButton('Water')
+        water_button = QPushButton("Water")
         water_button.setMaximumWidth(100)
         water_button.setMaximumHeight(30)
-        water_button.clicked.connect(lambda: self.set_draw_mode('water'))
+        water_button.clicked.connect(lambda: self.set_draw_mode("water"))
         main_layout.addWidget(water_button, 1, 0, 1, 1)
 
-        fire_button = QPushButton('Fire')
+        fire_button = QPushButton("Fire")
         fire_button.setMaximumWidth(100)
         fire_button.setMaximumHeight(30)
-        fire_button.clicked.connect(lambda: self.set_draw_mode('fire'))
+        fire_button.clicked.connect(lambda: self.set_draw_mode("fire"))
         main_layout.addWidget(fire_button, 1, 1, 1, 1)
 
-        wall_button = QPushButton('Wall')
+        wall_button = QPushButton("Wall")
         wall_button.setMaximumWidth(100)
         wall_button.setMaximumHeight(30)
-        wall_button.clicked.connect(lambda: self.set_draw_mode('wall'))
+        wall_button.clicked.connect(lambda: self.set_draw_mode("wall"))
         main_layout.addWidget(wall_button, 1, 2, 1, 1)
 
-        boost_button = QPushButton('Boost')
+        boost_button = QPushButton("Boost")
         boost_button.setMaximumWidth(100)
         boost_button.setMaximumHeight(30)
-        boost_button.clicked.connect(lambda: self.set_draw_mode('boost'))
+        boost_button.clicked.connect(lambda: self.set_draw_mode("boost"))
         main_layout.addWidget(boost_button, 1, 3, 1, 1)
 
-        spikes_button = QPushButton('Spikes')
+        spikes_button = QPushButton("Spikes")
         spikes_button.setMaximumWidth(100)
         spikes_button.setMaximumHeight(30)
-        spikes_button.clicked.connect(lambda: self.set_draw_mode('spikes'))
+        spikes_button.clicked.connect(lambda: self.set_draw_mode("spikes"))
         main_layout.addWidget(spikes_button, 1, 4, 1, 1)
 
-        save_button = QPushButton('Save')
+        save_button = QPushButton("Save")
         save_button.setMaximumWidth(100)
         save_button.setMaximumHeight(30)
-        save_button.clicked.connect(lambda: self.save_to_text_file('MapEditorArena.txt'))
+        save_button.clicked.connect(
+            lambda: self.save_to_text_file("MapEditorArena.txt")
+        )
         main_layout.addWidget(save_button, 1, 5, 1, 1)
 
         # Add an empty stretch to push buttons to the bottom
         main_layout.setRowStretch(0, 1)
 
-        #change size, position of buttons
+        # change size, position of buttons
         water_button.setGeometry(50, 50, 100, 30)
         fire_button.setGeometry(50, 100, 100, 30)
         wall_button.setGeometry(50, 150, 100, 30)
@@ -70,26 +72,25 @@ class MapEditor(QWidget):
         self.shapes = []
         self.start_point = None
 
-
-
-    #converts the drawn arena into a textfile and saves this
+    # converts the drawn arena into a textfile and saves this
     def save_to_text_file(self, filename):
-        image = self.grab().toImage()    #makes screenshot of window
-        image = image.scaled(100, 100)   #scales to size 100x100 so that 1 pixel is 1 tile
+        image = self.grab().toImage()  # makes screenshot of window
+        image = image.scaled(
+            100, 100
+        )  # scales to size 100x100 so that 1 pixel is 1 tile
 
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             for y in range(image.height()):
                 for x in range(image.width()):
                     color = image.pixelColor(x, y)
                     if color == Qt.red:
-                        file.write('f')
+                        file.write("f")
                     elif color == Qt.blue:
-                        file.write('a')
+                        file.write("a")
                     elif color == Qt.black:
-                        file.write('b')
+                        file.write("b")
                     else:
-                        file.write('n')
-
+                        file.write("n")
 
     def paintEvent(self, event):
         # Create a QPainter object
@@ -97,19 +98,19 @@ class MapEditor(QWidget):
 
         # Draw the shapes
         for shape in self.shapes:
-            if shape[0] == 'water':
+            if shape[0] == "water":
                 pen = QPen(Qt.blue)
                 brush = QBrush(Qt.blue)
-            elif shape[0] == 'fire':
+            elif shape[0] == "fire":
                 pen = QPen(Qt.red)
                 brush = QBrush(Qt.red)
-            elif shape[0] == 'wall':
+            elif shape[0] == "wall":
                 pen = QPen(Qt.black)
                 brush = QBrush(Qt.black)
-            elif shape[0] == 'spikes':
+            elif shape[0] == "spikes":
                 pen = QPen(Qt.gray)
                 brush = QBrush(Qt.gray)
-            elif shape[0] == 'boost':
+            elif shape[0] == "boost":
                 pen = QPen(Qt.green)
                 brush = QBrush(Qt.green)
 
@@ -130,7 +131,13 @@ class MapEditor(QWidget):
             width = event.pos().x() - self.start_point.x()
             height = event.pos().y() - self.start_point.y()
 
-            shape = (self.draw_mode, self.start_point.x(), self.start_point.y(), width, height)
+            shape = (
+                self.draw_mode,
+                self.start_point.x(),
+                self.start_point.y(),
+                width,
+                height,
+            )
             self.shapes.append(shape)
             self.update()
 
@@ -139,13 +146,20 @@ class MapEditor(QWidget):
             width = event.pos().x() - self.start_point.x()
             height = event.pos().y() - self.start_point.y()
 
-            shape = (self.draw_mode, self.start_point.x(), self.start_point.y(), width, height)
+            shape = (
+                self.draw_mode,
+                self.start_point.x(),
+                self.start_point.y(),
+                width,
+                height,
+            )
             self.shapes.append(shape)
             self.update()
 
         self.start_point = None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     editor = MapEditor()
     editor.show()
