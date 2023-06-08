0372ff51ed4d7ce8a8c96d3805f72f7aaf60dd84
roadsidepicnic1 <roadsidepicnic1@users.noreply.github.com>
2023-06-08 18:58:56 +0000

    Format code with black

--- RoboArena2/MapEditor.py
+++ RoboArena2/MapEditor.py
@@ -1,10 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QBrush, QPen, QPixmap
-from PyQt5.QtCore import Qt
-from PyQt5.uic import loadUi
-from Menus import ExtrasMenu
-
 import sys
+
+from Menus import ExtrasMenu
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QPainter, QPen
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+
 
 class MapEditor(QMainWindow):
     def __init__(self):
@@ -22,19 +23,23 @@ class MapEditor(QMainWindow):
         # create buttons
         self.draw_mode = None
 
-        self.water_button.clicked.connect(lambda: self.set_draw_mode('water'))
+        self.water_button.clicked.connect(lambda: self.set_draw_mode("water"))
 
-        self.fire_button.clicked.connect(lambda: self.set_draw_mode('fire'))
+        self.fire_button.clicked.connect(lambda: self.set_draw_mode("fire"))
 
-        self.wall_button.clicked.connect(lambda: self.set_draw_mode('wall'))
+        self.wall_button.clicked.connect(lambda: self.set_draw_mode("wall"))
 
-        self.boost_button.clicked.connect(lambda: self.set_draw_mode('boost'))
+        self.boost_button.clicked.connect(lambda: self.set_draw_mode("boost"))
 
-        self.spikes_button.clicked.connect(lambda: self.set_draw_mode('spikes'))
+        self.spikes_button.clicked.connect(
+            lambda: self.set_draw_mode("spikes")
+        )
 
         self.new_button.clicked.connect(self.new_map)
 
-        self.save_button.clicked.connect(lambda: self.save_to_text_file('MapEditorArena.txt'))
+        self.save_button.clicked.connect(
+            lambda: self.save_to_text_file("MapEditorArena.txt")
+        )
 
         self.undo_button.clicked.connect(self.undo_last_shape)
 
@@ -55,28 +60,29 @@ class MapEditor(QMainWindow):
             self.shapes.pop()
             self.update()
 
-    #converts the drawn arena into a textfile and saves it
+    # converts the drawn arena into a textfile and saves it
     def save_to_text_file(self, filename):
-        image = self.grab().toImage()    #makes screenshot of window
-        image = image.scaled(100, 100)   #scales to size 100x100 so that 1 pixel is 1 tile
+        image = self.grab().toImage()  # makes screenshot of window
+        image = image.scaled(
+            100, 100
+        )  # scales to size 100x100 so that 1 pixel is 1 tile
 
-        with open(filename, 'w') as file:
-            for y in range(100):  #iterate through every pixel
+        with open(filename, "w") as file:
+            for y in range(100):  # iterate through every pixel
                 for x in range(100):
                     color = image.pixelColor(x, y)
                     if color == Qt.red:
-                        file.write('f')
+                        file.write("f")
                     elif color == Qt.blue:
-                        file.write('a')
+                        file.write("a")
                     elif color == Qt.black:
-                        file.write('b')
+                        file.write("b")
                     elif color == Qt.gray:
-                        file.write('s')
+                        file.write("s")
                     elif color == Qt.green:
-                        file.write('b')
+                        file.write("b")
                     else:
-                        file.write('n')
-
+                        file.write("n")
 
     def paintEvent(self, event):
         # Create a QPainter object
@@ -84,23 +90,23 @@ class MapEditor(QMainWindow):
 
         # Draw the shapes
         for shape in self.shapes:
-            if shape[0] == 'water':
+            if shape[0] == "water":
                 pen = QPen(Qt.blue)
                 brush = QBrush(Qt.blue)
                 # Draw the graphical tile
                 if len(shape) > 5:
                     graphical_tile = shape[5]
                     painter.drawPixmap(shape[1], shape[2], graphical_tile)
-            elif shape[0] == 'fire':
+            elif shape[0] == "fire":
                 pen = QPen(Qt.red)
                 brush = QBrush(Qt.red)
-            elif shape[0] == 'wall':
+            elif shape[0] == "wall":
                 pen = QPen(Qt.black)
                 brush = QBrush(Qt.black)
-            elif shape[0] == 'spikes':
+            elif shape[0] == "spikes":
                 pen = QPen(Qt.gray)
                 brush = QBrush(Qt.gray)
-            elif shape[0] == 'boost':
+            elif shape[0] == "boost":
                 pen = QPen(Qt.green)
                 brush = QBrush(Qt.green)
 
@@ -112,19 +118,19 @@ class MapEditor(QMainWindow):
         # Draw the temporary shape during mouse move
         if self.temp_shape:
             shape = self.temp_shape
-            if shape[0] == 'water':
+            if shape[0] == "water":
                 pen = QPen(Qt.blue)
                 brush = QBrush(Qt.blue)
-            elif shape[0] == 'fire':
+            elif shape[0] == "fire":
                 pen = QPen(Qt.red)
                 brush = QBrush(Qt.red)
-            elif shape[0] == 'wall':
+            elif shape[0] == "wall":
                 pen = QPen(Qt.black)
                 brush = QBrush(Qt.black)
-            elif shape[0] == 'spikes':
+            elif shape[0] == "spikes":
                 pen = QPen(Qt.gray)
                 brush = QBrush(Qt.gray)
-            elif shape[0] == 'boost':
+            elif shape[0] == "boost":
                 pen = QPen(Qt.green)
                 brush = QBrush(Qt.green)
 
@@ -133,18 +139,22 @@ class MapEditor(QMainWindow):
 
             painter.drawRect(*shape[1:])
 
-
     def mousePressEvent(self, event):
         if self.draw_mode is not None:
             self.start_point = event.pos()
-
 
     def mouseMoveEvent(self, event):
         if self.draw_mode is not None and self.start_point is not None:
             width = event.pos().x() - self.start_point.x()
             height = event.pos().y() - self.start_point.y()
 
-            self.temp_shape = (self.draw_mode, self.start_point.x(), self.start_point.y(), width, height)
+            self.temp_shape = (
+                self.draw_mode,
+                self.start_point.x(),
+                self.start_point.y(),
+                width,
+                height,
+            )
             self.update()
 
     def mouseReleaseEvent(self, event):
@@ -152,7 +162,13 @@ class MapEditor(QMainWindow):
             width = event.pos().x() - self.start_point.x()
             height = event.pos().y() - self.start_point.y()
 
-            shape = (self.draw_mode, self.start_point.x(), self.start_point.y(), width, height)
+            shape = (
+                self.draw_mode,
+                self.start_point.x(),
+                self.start_point.y(),
+                width,
+                height,
+            )
             self.shapes.append(shape)
             self.update()
 
@@ -171,7 +187,8 @@ class MapEditor(QMainWindow):
     def load_map_txt(self):
         print("totdo")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MapEditor()
     window.show()
