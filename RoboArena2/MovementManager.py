38b410947efcc1a300536b388fd51aeb9d21d5a6
Marcel John <marcel.john.s@t-online.de>
2023-06-29 17:07:59 +0200

    added dash Ability

--- RoboArena2/MovementManager.py
+++ RoboArena2/MovementManager.py
@@ -1,11 +1,13 @@
 from BasicRobot import BasicRobot, MovementTyp
 from PyQt5.QtCore import Qt
+import itertools
 
 
 class MovementManager_:
     waveConter = 0
     weaponsCurrentlyShoot = False
     ticksToNextShoot = 0
+    dashcooldown = 0
 
     def __init__(self, robot: BasicRobot):
         self.robot = robot
@@ -41,17 +43,23 @@ class MovementManager_:
             self.waveConter = self.waveConter % 100
 
     def handleInputPlayer1(self, keys: dict):
+        dashDistance = 10
+        dashcooldowntime = 10
         self.reduceTimerToShoot()
+        self.reduceAbilityCooldown()
         moveForward = Qt.Key.Key_W
         moveBack = Qt.Key.Key_S
         turnLeft = Qt.Key.Key_A
         turnRight = Qt.Key.Key_D
         shootWeapon = Qt.Key.Key_F
+        dashKey = Qt.Key.Key_Shift
         PressedMoveForward = moveForward in keys and keys[moveForward]
         PressedMoveBack = moveBack in keys and keys[moveBack]
         PressedTurnLeft = turnLeft in keys and keys[turnLeft]
         PressedTurnRight = turnRight in keys and keys[turnRight]
         PressedShootWeapon = shootWeapon in keys and keys[shootWeapon]
+        PressedDash = dashKey in keys and keys[dashKey]
+        dashcooldownNotActive = self.dashcooldown < 1
 
         moveVec = 0
         rotVec = 0
@@ -76,7 +84,25 @@ class MovementManager_:
             self.weaponsCurrentlyShoot = False
 
         self.robot.weaponsCurrentlyShoot = self.weaponsCurrentlyShoot
+        if PressedDash and dashcooldownNotActive:
+            if PressedMoveBack :
+                self.dashAbility(dashDistance, -1)
+                self.dashcooldown = dashcooldowntime
+            else:
+                self.dashAbility(dashDistance, 1)
+                self.dashcooldown = dashcooldowntime
+
 
     def reduceTimerToShoot(self) -> None:
         if self.ticksToNextShoot > 0:
             self.ticksToNextShoot -= 1
+
+    def dashAbility(self, distance : int, vec : int) -> None:
+        tick = 1 / 30
+        rotation = 0
+        for i in itertools.repeat(None, distance):
+            self.robot.tick(vec, rotation, tick)
+
+    def reduceAbilityCooldown(self) -> None:
+        if self.dashcooldown > 0:
+            self.dashcooldown -= 1
