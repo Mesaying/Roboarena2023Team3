d2e7039252a3f396d9486acba7e7621698fcd107
roadsidepicnic1 <roadsidepicnic1@users.noreply.github.com>
2023-07-04 09:40:08 +0000

    Format code with black

--- RoboArena2/Terrain.py
+++ RoboArena2/Terrain.py
@@ -61,12 +61,14 @@ class normal(terrain):
     type = "n"
     imagePath = "TileImages/Normal_tile.png"
 
+
 class player1(terrain):
     movement = 1.0
     solid = 0
     damage = 0
     type = "n"
     imagePath = "TileImages/Normal_tile.png"
+
 
 class player2(terrain):
     movement = 1.0
