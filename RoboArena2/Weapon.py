0ae93158edac88914cf96939a20e3526a3f5ca0e
Marcel John <marcel.john.s@t-online.de>
2023-07-14 14:35:57 +0200

    added Projectile weapos

--- RoboArena2/Weapon.py
+++ RoboArena2/Weapon.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 
 
 class WeaponTyp(Enum):
@@ -11,6 +12,10 @@ class WeaponName(Enum):
     basicLaser = "basicLaser"
     strongLaser = "strongLaser"
     sniper = "sniper"
+    basicProjectile = "basicProjectile"
+    minigun = "minigun"
+    bigBullet = "bigBullet"
+    curveBall = "curveBall"
 
 
 class Weapon:
@@ -20,7 +25,66 @@ class Weapon:
         self.ticksToNextShoot = 10
         self.typ = WeaponTyp.hitscan
         self.name = name
+        self.projectileSpeed = 10
+        self.extraTurnig = 0
+        self.listOfPositionForProjectils = []
+        self.listOfVectorsForProjectils = []
+        self.listOfDirections = []
         self.getWeaponStats(name)
+
+    def addProjectilePosition(self, x : int, y : int) -> None:
+        self.listOfPositionForProjectils.append([x, y])
+    
+    def addProjectileVector(self, direction : int) -> None:
+        xVelocity = (
+            (math.cos(math.radians(direction)))
+            * self.projectileSpeed
+        )
+        xVelocity = int(xVelocity)
+        yVelocity = (
+            (math.sin(math.radians(direction)))
+            * self.projectileSpeed
+        )
+        yVelocity = int(yVelocity)
+        self.listOfVectorsForProjectils.append([xVelocity, yVelocity])
+        self.listOfDirections.append(direction + self.extraTurnig)
+
+    def newXVelocity(self, posInList : int) -> int:
+        xVelocity = (
+            (math.cos(math.radians(self.listOfDirections[posInList])))
+            * self.projectileSpeed
+        )
+        xVelocity = int(xVelocity)
+        self.listOfDirections[posInList] += self.extraTurnig
+        return xVelocity
+    
+    def newYVelocity(self, posInList : int) -> int:
+        yVelocity = (
+            (math.sin(math.radians(self.listOfDirections[posInList])))
+            * self.projectileSpeed
+        )
+        yVelocity = int(yVelocity)
+        self.listOfDirections[posInList] += self.extraTurnig
+        return yVelocity
+
+    def moveProjectils(self) -> None:
+        if(len(self.listOfPositionForProjectils) == len(self.listOfVectorsForProjectils)):
+            for i in range(len(self.listOfPositionForProjectils)):
+                self.listOfPositionForProjectils[i][0] += self.listOfVectorsForProjectils[i][0]
+                self.listOfPositionForProjectils[i][1] += self.listOfVectorsForProjectils[i][1]
+                if(self.extraTurnig > 0):
+                    self.listOfVectorsForProjectils[i][0] = self.newXVelocity(i)
+                    self.listOfVectorsForProjectils[i][1] = self.newYVelocity(i)
+
+    def deleatProjectile(self, PosInList : int) -> None:
+        try:
+            del self.listOfPositionForProjectils[PosInList]
+        except Exception:
+            pass
+        try:
+            del self.listOfVectorsForProjectils[PosInList]
+        except Exception:
+            pass
 
     def getWeaponStats(self, name: WeaponName) -> None:
         match (name):
@@ -44,3 +108,28 @@ class Weapon:
                 self.size = 2000
                 self.ticksToNextShoot = 15
                 self.typ = WeaponTyp.hitscan
+            case WeaponName.basicProjectile:
+                self.damage = 10
+                self.size = 5
+                self.ticksToNextShoot = 1
+                self.projectileSpeed = 40
+                self.typ = WeaponTyp.projectile
+            case WeaponName.minigun:
+                self.damage = 7
+                self.size = 5
+                self.ticksToNextShoot = 0
+                self.projectileSpeed = 40
+                self.typ = WeaponTyp.projectile
+            case WeaponName.bigBullet:
+                self.damage = 20
+                self.size = 40
+                self.ticksToNextShoot = 7
+                self.projectileSpeed = 30
+                self.typ = WeaponTyp.projectile
+            case WeaponName.curveBall:
+                self.damage = 40
+                self.size = 20
+                self.ticksToNextShoot = 5
+                self.projectileSpeed = 30
+                self.extraTurnig = 10
+                self.typ = WeaponTyp.projectile
